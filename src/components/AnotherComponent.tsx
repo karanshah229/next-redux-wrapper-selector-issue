700011de66713819677d6480f9067065a4a5ba14
Karan Shah <42066323+karanshah229@users.noreply.github.com>
2023-10-07 21:34:54 +0530

    feat: Add next-redux-wrapper

--- src/components/AnotherComponent.tsx
+++ src/components/AnotherComponent.tsx
@@ -4,12 +4,10 @@ import store from "../store";
 
 export function AnotherComponent() {
 	useEffect(() => {
-		// setTimeout(() => {
 		if (typeof window !== undefined) {
 			console.log("Dispatched");
 			store.dispatch(fetchCounterDataThunk());
 		}
-		// }, 5000);
 	}, []);
 
 	return "Child Component calls Async Thunk to fetch data and put in store";
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import store from "../store";
+import { wrapper } from "../store";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, ...rest }: AppProps) {
+	const {
+		store,
+		props: { pageProps },
+	} = wrapper.useWrappedStore(rest);
+
 	return (
 		<Provider store={store}>
 			<Component {...pageProps} />
